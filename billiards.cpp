7461a2a86e7f0aa05ba9760ef08e85bbaf2d874e
Dídac Sementé Fernández <didac.semente@gmail.com>
2016-02-15 15:01:47 +0100

    Added test bounce code

--- billiards.cpp
+++ billiards.cpp
@@ -3,6 +3,7 @@
 //-----------------------------------------------
 
 #include <GL/freeglut.h>
+#include <cstdlib>
 
 //-----------------------------------------------
 
@@ -15,6 +16,8 @@
 void initContext();
 void initGraphicContext(int, char**);
 void cleanContext();
+
+void testBounceBall(long);
 
 //-----------------------------------------------
 // -- DISPLAY METHODS
@@ -34,11 +37,22 @@ Point3 *ballPosition;
 Vector3 *ballVelocity;
 RGBColor *color;
 
+bool bounce = false;
+
+float worldx = 2.0, worldy = 1.0;
+
+double speedx, speedy;
+
+long elapsedTime, deltaTime, lastElapsed;
+
 //-----------------------------------------------
 // -- MAIN PROCEDURE
 //-----------------------------------------------
 
 int main(int argc,char *argv[]) {
+
+    speedx = (double) (rand() % 20) / 10.0;
+    speedy = (double) (rand() % 20) / 10.0;
 
     initContext();
 
@@ -49,9 +63,11 @@ int main(int argc,char *argv[]) {
     
 }
 
+//-----------------------------------------------
+
 void initContext() {
     ballPosition = new Point3(0.5, 0.5, 0.0);
-    ballVelocity = new Vector3();
+    ballVelocity = new Vector3(speedx, speedy, 0.0);
     color = new RGBColor(123, 123, 123);
     ball = new Ball(1.0, *ballPosition, *ballVelocity, 0.1, *color);
 
@@ -68,7 +84,7 @@ void initGraphicContext(int argc, char **argv) {
     glutCreateWindow("Billiards Game");
 
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0, 2, 0, 1);
+    gluOrtho2D(0, worldx, 0, worldy);
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
@@ -87,14 +103,59 @@ void cleanContext() {
 }
 
 void display() {
+    glClearColor(1.0,1.0,1.0,0.0); // Clear screen color
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); // Clear
+    
     ball->draw();
     glutSwapBuffers();
 }
 
 void keyboard(uchar c, int x, int y) {
-
+    char key = c;
+    switch(key) {
+        case ' ':
+            bounce = !bounce;
+            break;
+        default:
+            break;
+    }
+    glutPostRedisplay();
 }
 
 void idle() {
+    elapsedTime = glutGet(GLUT_ELAPSED_TIME);
+    deltaTime = elapsedTime - lastElapsed;
 
+    if(bounce) {
+        testBounceBall(deltaTime);
+    }
+
+    lastElapsed = elapsedTime;
+
+    glutPostRedisplay();
+}
+
+void testBounceBall(long delta) {
+    double deltaSec = ((double) delta) / 1000.0;
+    double x, y;
+    bool changed = false;
+    Vector3 vector;
+
+    Point3 currentPos = ball->integrate(deltaSec);
+    vector = ball->getVelocity();
+    x = currentPos.getX();
+    y = currentPos.getY();
+
+    if(x <= 0.0 || x > worldx) {
+        vector.setX(vector.getX() * -1.0);
+        changed = true;
+    }
+
+    if(y <= 0.0 || y > worldy) {
+        vector.setY(vector.getY() * -1.0);
+        changed = true;
+    }
+    if(changed) {
+        ball->setVelocity(vector);
+    }
 }
